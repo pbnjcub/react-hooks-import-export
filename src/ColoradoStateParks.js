fddeb0dd5baead8f1252eb846c90a3f2fa9ad84e
Paul Jung <pbnjcub@gmail.com>
2022-08-27 09:59:17 -0400

    commit tree 9889ae1528e1a1dc949e73b498228f8ca59b5df5

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,12 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import * as RMFunctions from "./parks/RockyMountain"
+
+console.log(RMFunctions.trees);
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks;
